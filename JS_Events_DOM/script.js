c4c133c9c5e22137845b7cd797a4f13609a1bace
Yeonwoo <yeonwoobae2003@gmail.com>
2025-04-02 19:16:16 -0700

    added in the function to generate random background colors

--- JS_Events_DOM/script.js
+++ JS_Events_DOM/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function()
 {
     let onLoadElement = document.getElementById("onload");
 
-    onLoadElement.innerHTML = "<h1> I loaded becasue the DOM was fully rendered </h1>"
+    onLoadElement.innerHTML = "<h1> I loaded because the DOM was fully rendered </h1>"
 });
 
 let directionElement = document.getElementById("direction");
@@ -18,3 +18,17 @@ document.addEventListener("click", function(e)
 
     clickMeElement.innerText = "You clicked me!"
 });
+
+function randomColorGenerator() 
+{
+    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+};
+
+function setColor()
+{
+    return document.body.style.backgroundColor = randomColorGenerator()
+}
+
+const RANDOMCOLORS = document.getElementById("randomColorBtn")
+
+RANDOMCOLORS.addEventListener("click", setColor)
